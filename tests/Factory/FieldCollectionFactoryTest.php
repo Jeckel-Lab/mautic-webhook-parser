ac3c197fe6f5dd25e6447cb2fe46d0de824dff94
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2023-10-26 15:45:22 +0200

    Upgrade tests

--- tests/Factory/FieldCollectionFactoryTest.php
+++ tests/Factory/FieldCollectionFactoryTest.php
@@ -42,7 +42,7 @@ class FieldCollectionFactoryTest extends TestCase
         self::assertSame('1 rue du port', $field->value);
     }
 
-    public function testFactoryWithLocalFieldsShouldReturnCollection(): void
+    public function testFactoryWithSingleLocalFieldShouldReturnCollection(): void
     {
         $data = [
             'preferred_locale' => [
@@ -57,6 +57,47 @@ class FieldCollectionFactoryTest extends TestCase
         $collection = (new FieldCollectionFactory())->constructFromJson($data);
         self::assertCount(1, $collection);
         self::assertNull($collection->get('address1'));
+
+        $field = $collection->get('preferred_locale');
+        self::assertInstanceOf(LocaleField::class, $field);
+        self::assertSame('preferred_locale', $field->alias);
+        self::assertSame(FieldTypeGroup::CORE, $field->group);
+        self::assertSame(FieldTypeId::from(16), $field->id);
+        self::assertSame('Preferred Locale', $field->label);
+        self::assertSame('fr_FR', (string) $field->value);
+    }
+
+    public function testFactoryWithMultipleFieldsShouldReturnCollection(): void
+    {
+        $data = [
+            'address1' => [
+                'alias' => 'address1',
+                'group' => 'core',
+                'id' => 10,
+                'label' => 'Address Line 1',
+                'type' => 'text',
+                'value' => '1 rue du port'
+            ],
+            'preferred_locale' => [
+                'alias' => 'preferred_locale',
+                'group' => 'core',
+                'id' => 16,
+                'label' => 'Preferred Locale',
+                'type' => 'locale',
+                'value' => 'fr_FR'
+            ]
+        ];
+
+        $collection = (new FieldCollectionFactory())->constructFromJson($data);
+        self::assertCount(2, $collection);
+
+        $field = $collection->get('address1');
+        self::assertInstanceOf(StringField::class, $field);
+        self::assertSame('address1', $field->alias);
+        self::assertSame(FieldTypeGroup::CORE, $field->group);
+        self::assertSame(FieldTypeId::from(10), $field->id);
+        self::assertSame('Address Line 1', $field->label);
+        self::assertSame('1 rue du port', $field->value);
 
         $field = $collection->get('preferred_locale');
         self::assertInstanceOf(LocaleField::class, $field);
